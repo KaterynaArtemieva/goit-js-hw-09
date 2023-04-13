d86a65fa4daecf1c7a1f54c59e44828eeb816b6a
Kateryna Artemieva <moruga.katya@gmail.com>
2023-04-13 16:52:36 +0100

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = evt.currentTarget;
+
+  for (
+    i = 1, j = Number(delay.value);
+    i <= amount.value;
+    i += 1, j += Number(step.value)
+  ) {
+    createPromise(i, j);
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise(() => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
